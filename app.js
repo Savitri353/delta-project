c9c69979663593ae8d801c3263a342a675f0ebc0
savitri <savitrigamit123456@gmail.com>
2025-04-11 13:10:16 +0530

    Adding restriction for review deletion

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.use((req,res,next)=> {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currentUser = req.user;
+    console.log(res.locals.currentUser);
     next();
 });
 
